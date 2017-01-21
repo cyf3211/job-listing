8f85e1092e6bb2da760a2dc2d748b2ee08d20d8a
陈扬帆 <cyf3211@qq.com>
2017-01-21 13:24:08 +0800

    index

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,9 +1,9 @@
 module JobsHelper
   def render_job_status(job)
     if job.is_hidden
-      content_tag(:span,"", :class => "fa fa-lock")
+      content_tag(:span,"", :style=>"color:#f0ad4e", :class => "fa fa-lock")
     else
-      content_tag(:span,"", :class => "fa fa-globe")
+      content_tag(:span,"",:style=>"color:#5cb85c", :class => "fa fa-globe")
     end
   end
 end
